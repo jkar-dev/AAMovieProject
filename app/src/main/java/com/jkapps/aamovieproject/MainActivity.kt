ca6a01a952d93437290e144047426261478cb251
Julia Karavaeva <karavaeva.jul@gmail.com>
2020-11-21 14:14:34 +0300

    add details activity

--- app/src/main/java/com/jkapps/aamovieproject/MainActivity.kt
+++ app/src/main/java/com/jkapps/aamovieproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jkapps.aamovieproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openDetailsActivity()
+    }
+
+    private fun openDetailsActivity() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
