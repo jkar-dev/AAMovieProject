d5707ec088d38e328f5593e8f1b4b9fadcc63ba6
Julia Karavaeva <karavaeva.jul@gmail.com>
2020-11-25 17:54:40 +0300

    delete activity, create list layout

--- app/src/main/java/com/jkapps/aamovieproject/FragmentMoviesDetails.kt
+++ app/src/main/java/com/jkapps/aamovieproject/FragmentMoviesDetails.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class MovieDetailsActivity : Fragment() {
+class FragmentMoviesDetails : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movies_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.tv_back).setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
     }
 }
--- app/src/main/java/com/jkapps/aamovieproject/FragmentMoviesList.kt
+++ app/src/main/java/com/jkapps/aamovieproject/FragmentMoviesList.kt
@@ -11,4 +11,18 @@ class FragmentMoviesList : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val movie = view.findViewById<View>(R.id.movie_item)
+        movie.setOnClickListener {
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, FragmentMoviesDetails())
+                .addToBackStack(null)
+                .commit()
+        }
+
+    }
 }
