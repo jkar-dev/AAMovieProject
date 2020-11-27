72342c1c61cae4f7eec2d9680b1d3e264b71ae4b
Julia Karavaeva <karavaeva.jul@gmail.com>
2020-11-27 09:34:06 +0300

    add navigation

--- app/src/main/java/com/jkapps/aamovieproject/FragmentMoviesList.kt
+++ app/src/main/java/com/jkapps/aamovieproject/FragmentMoviesList.kt
@@ -1,5 +1,6 @@
 package com.jkapps.aamovieproject
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesList : Fragment() {
+    private var navListener : NavigationListener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
@@ -17,12 +19,17 @@ class FragmentMoviesList : Fragment() {
 
         val movie = view.findViewById<View>(R.id.movie_item)
         movie.setOnClickListener {
-            requireActivity().supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.container, FragmentMoviesDetails())
-                .addToBackStack(null)
-                .commit()
+            navListener?.openDetails()
         }
+    }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is NavigationListener) navListener = context
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        navListener = null
     }
 }
--- app/src/main/java/com/jkapps/aamovieproject/MainActivity.kt
+++ app/src/main/java/com/jkapps/aamovieproject/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.jkapps.aamovieproject
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +15,17 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.container, FragmentMoviesList())
                 .commit()
         }
+    }
+
+    override fun pop() {
+        supportFragmentManager.popBackStack()
+    }
+
+    override fun openDetails() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, FragmentMoviesDetails())
+            .addToBackStack(null)
+            .commit()
     }
 }
