2a6a72fec8f1e6358ff98f84282e93c1f87c5c5b
Julia Karavaeva <karavaeva.jul@gmail.com>
2020-12-11 13:40:45 +0300

    set up list recycler

--- app/src/main/java/com/jkapps/aamovieproject/ui/list/FragmentMoviesList.kt
+++ app/src/main/java/com/jkapps/aamovieproject/ui/list/FragmentMoviesList.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jkapps.aamovieproject.NavigationListener
 import com.jkapps.aamovieproject.R
+import com.jkapps.aamovieproject.data.DataSource
+import com.jkapps.aamovieproject.ui.MovieListAdapter
 
 class FragmentMoviesList : Fragment() {
-    private var navListener : NavigationListener? = null
+    private var navListener: NavigationListener? = null
+    private val adapter = MovieListAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
@@ -18,10 +23,16 @@ class FragmentMoviesList : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpRecycler()
+        adapter.setMovies(DataSource.movies)
 
-        val movie = view.findViewById<View>(R.id.movie_item)
-        movie.setOnClickListener {
-            navListener?.openDetails()
+    }
+
+    private fun setUpRecycler() {
+        val recycler = requireView().findViewById<RecyclerView>(R.id.rv_movies)
+        recycler.apply {
+            adapter = this@FragmentMoviesList.adapter
+            layoutManager = GridLayoutManager(requireContext(), 2)
         }
     }
 
